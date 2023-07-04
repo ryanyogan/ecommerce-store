afe065968f3901baa001be5907e8d9aee17447b9
Ryan Yogan <ryanyogan@gmail.com>
2023-07-04 17:52:55 -0500

    allow multiple images in gallery picker

--- src/components/ui/product-card.tsx
+++ src/components/ui/product-card.tsx
@@ -3,6 +3,7 @@
 import { Product } from "@/types";
 import { Expand, ShoppingCart } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import Currency from "./currency";
 import IconButton from "./icon-button";
 
@@ -11,8 +12,17 @@ interface ProductCardProps {
 }
 
 export default function ProductCard({ data }: ProductCardProps) {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`);
+  };
+
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+    >
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image
           className="aspect-square object-cover rounded-md"
