1b716462c8cd3bd532dbb8726a9b930df9ae2fa7
Ryan Yogan <ryanyogan@gmail.com>
2023-07-04 19:55:34 -0500

    wip - mobile filters

--- src/app/(routes)/category/[categoryId]/page.tsx
+++ src/app/(routes)/category/[categoryId]/page.tsx
@@ -4,6 +4,7 @@ import { getProducts } from "@/actions/get-products";
 import { getSizes } from "@/actions/get-sizes";
 import Billboard from "@/components/billboard";
 import Filter from "@/components/filter";
+import MobileFilters from "@/components/filter/mobile-filters";
 import Container from "@/components/ui/container";
 import NoResults from "@/components/ui/no-results";
 import ProductCard from "@/components/ui/product-card";
@@ -43,11 +44,12 @@ export default async function CategoryPage({
 
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+            <MobileFilters sizes={sizes} colors={colors} />
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
               <Filter valueKey="colorId" name="Colors" data={colors} />
             </div>
-            <div className="mt6 lg:col-span-4 lg:mt-0">
+            <div className="mt-6 lg:col-span-4 lg:mt-0">
               {products.length === 0 && <NoResults />}
 
               <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
