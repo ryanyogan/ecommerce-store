2a92d2df00d5bbab464c3bf05abf9499fad17415
Ryan Yogan <ryanyogan@gmail.com>
2023-07-04 20:43:57 -0500

    do not cache requests long

--- src/actions/get-category.tsx
+++ src/actions/get-category.tsx
@@ -3,7 +3,7 @@ import { Category } from "@/types";
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
 export async function getCategory(id: string): Promise<Category> {
-  const res = await fetch(`${URL}/${id}`);
+  const res = await fetch(`${URL}/${id}`, { next: { revalidate: 10 } });
 
   return res.json();
 }
